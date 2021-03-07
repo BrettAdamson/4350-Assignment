1a0fbfa4ae49c00a01423a6eb3e140fc7eb1e34d
Brett Adamson <brettmadamson.gmail.com>
2021-03-07 01:39:06 -0600

    got both requests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,12 +5,7 @@
       <b-col class="mt-4 pt-4">
         <h1>COMP 4350 Assignment</h1>
         <b-form-input v-model="form.tag" type="text" placeholder="Enter tag" required></b-form-input>
-        <b-button
-          type="submit"
-          variant="primary"
-          class="mt-4"
-          @click="queryMostRecent();queryHighestRated()"
-        >Submit</b-button>
+        <b-button type="submit" variant="primary" class="mt-4" @click="queryResults()">Submit</b-button>
       </b-col>
       <b-col></b-col>
     </b-row>
@@ -27,57 +22,37 @@ export default {
     };
   },
   methods: {
-    queryMostRecent() {
+    queryResults() {
       var now = new Date();
       var oneWeekAgo = new Date();
       oneWeekAgo.setDate(now.getDate() - 7);
       now = Math.floor(now.getTime() / 1000);
       oneWeekAgo = Math.floor(oneWeekAgo.getTime() / 1000);
       var tag = encodeURIComponent(this.form.tag);
-      this.$axios
-        .get(
-          "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=" +
-            oneWeekAgo +
-            "&todate=" +
-            now +
-            "&order=desc&sort=activity&tagged=" +
-            tag +
-            "&site=stackoverflow"
-        )
-        .then(function(response) {
-          // handle success
-          console.log(response.data);
-        })
-        .catch(function(error) {
-          // handle error
-          console.log(error);
-        });
-    },
-    queryHighestRated() {
-      var now = new Date();
-      var oneWeekAgo = new Date();
-      oneWeekAgo.setDate(now.getDate() - 7);
-      now = Math.floor(now.getTime() / 1000);
-      oneWeekAgo = Math.floor(oneWeekAgo.getTime() / 1000);
-      var tag = encodeURIComponent(this.form.tag);
-      this.$axios
-        .get(
-          "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=" +
-            oneWeekAgo +
-            "&todate=" +
-            now +
-            "&order=desc&sort=votes&tagged=" +
-            tag +
-            "&site=stackoverflow"
-        )
-        .then(function(response) {
-          // handle success
-          console.log(response.data);
-        })
-        .catch(function(error) {
-          // handle error
-          console.log(error);
-        });
+      var mostRecent =
+        "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=" +
+        oneWeekAgo +
+        "&todate=" +
+        now +
+        "&order=desc&sort=activity&tagged=" +
+        tag +
+        "&site=stackoverflow";
+      var highestRated =
+        "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=" +
+        oneWeekAgo +
+        "&todate=" +
+        now +
+        "&order=desc&sort=votes&tagged=" +
+        tag +
+        "&site=stackoverflow";
+
+        const recentRequest = this.$axios.get(mostRecent)
+        const ratedRequest = this.$axios.get(highestRated)
+
+        this.$axios.all([recentRequest,ratedRequest]).then(this.$axios.spread((recentResponse, ratedResponse) => {
+          console.log(recentResponse)
+          console.log(ratedResponse)
+        }))
     }
   }
 };
